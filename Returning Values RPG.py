6d9feead82b40c2545186f086e2696d06cfaca62
500kguser <102877038+500kguser@users.noreply.github.com>
2022-09-06 12:47:49 +0100

    Update Returning Values RPG.py

--- Returning Values RPG.py
+++ Returning Values RPG.py
@@ -1,11 +1,16 @@
-#Use the get_seconds function to work out the amount of seconds in 2 hours and 30 minutes..
-#then add this number to the amount of seconds in 45 minutes and 15 seconds.
+# Use the get_seconds function to work out the amount of seconds in 2 hours and 30 minutes..
+# then add this number to the amount of seconds in 45 minutes and 15 seconds.
 
-def hit_points(seconds):
-  hours = seconds // 3600
-  minutes = (seconds - hours * 3600) // 60
-  remaining_seconds = seconds - hours * 3600 - minutes * 30
-  return hours, minutes, remaining_seconds
+def hit_points(hp):
+    con = 10
+    max_hp = con * 10
+    current_hp = max_hp - damage_taken
+    damage_taken = enemy_damage
+    enemy_damage = enemy_str
+    enemy_str = 10
+    return con, max_hp, current_hp, damage_taken, enemy_damage, enemy_str
 
-hours, minutes, seconds = hit_points(4000)
-print(hours, minutes, seconds)
+    print(con, max_hp, current_hp, damage_taken, enemy_damage, enemy_str)
+    print(max_hp)
+    print(hit_points)
+    print(hp)
